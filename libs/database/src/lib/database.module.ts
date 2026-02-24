b2cab3f35079f85be68fcb0f380880504ff22875
vacrandomsample <malininstalker@mail.ru>
2026-02-24 14:16:16 +0300

    Новая микросервисная архитектура с NX и NestJS

---
+++ libs/database/src/lib/database.module.ts
@@ -0,0 +1,26 @@
+import { Module, Global } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { User } from './entities/user.entity';
+
+@Global() // опционально
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [User], // здесь перечисляем все сущности библиотеки
+        synchronize: configService.get('DB_SYNCHRONIZE') === 'true',
+      }),
+    }),
+  ],
+  exports: [TypeOrmModule], // если нужно, чтобы другие модули могли использовать репозитории
+})
+export class DatabaseModule {}
